9841451607f0b59d3b4ea7a5b022cc6fe0a74439
Adam Ratcliffe <adam.ratcliffe@tomtom.com>
2025-03-22 01:03:15 -0700

    Use latest Orbis map style.

--- tomtom-map-iframe/src/constants/MapStyles.js
+++ tomtom-map-iframe/src/constants/MapStyles.js
@@ -1,7 +1,7 @@
 const MapStyles = {
   street: {
     orbis:
-      "https://api.tomtom.com/maps/orbis/assets/styles/0.2.*/style.json?apiVersion=1&map=basic_street-light&trafficFlow=flow_relative-light&trafficIncidents=incidents_light",
+      "https://api.tomtom.com/maps/orbis/assets/styles/0.*/style.json?apiVersion=1&map=basic_street-light&trafficFlow=flow_relative-light&trafficIncidents=incidents_light",
     genesis:
       "https://api.tomtom.com/style/1/style/22.2.1-9?map=2/basic_street-light&traffic_incidents=2/incidents_light&traffic_flow=2/flow_relative-light"
   },
